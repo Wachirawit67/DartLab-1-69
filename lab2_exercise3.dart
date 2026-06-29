43f852b747703af9f9c5d5e0f48563f065616574
Wachirawit67 <basebasebase09@gmail.com>
2026-06-29 12:08:17 +0700

    List_Maps

---
+++ lab2_exercise3.dart
@@ -0,0 +1,21 @@
+void main() {
+  List<String> subjects = ['Network', 'Mobile App', 'Algorithm'];
+  print('Number of subjects: ${subjects.length}');
+  print('First subjects: ${subjects[0]}');
+  print('Last subjuects: ${subjects[subjects.length - 1]}');
+  
+  subjects.add('Game Design');
+  print('Updated subjects: $subjects');
+  print('---');
+  
+  Map<String, int> studentScores = {
+    'Network': 77,
+    'Mobile App': 67,
+  };
+  print('Score for Mobile App: ${studentScores['Mobile App']}');
+  
+  studentScores['Algorithm'] = 84;
+  print('Updated scores: $studentScores');
+  print('All subjects in map: ${studentScores.keys}');
+  print('All scores in map: ${studentScores.values}');
+}
